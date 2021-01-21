824c40c3f4e5488be98c88c92b5ba0cb6338caf8
Sergey <bjolyk@gmail.com>
2021-01-21 16:41:49 +0300

    Progress after the class of 21.01.2021 (204-1).

--- code/src/problem3_enh_mult_table/main.cpp
+++ code/src/problem3_enh_mult_table/main.cpp
@@ -14,9 +14,50 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 
+#include <cstdint>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+
 int main()
 {
-    // TODO: input your code here
+    cout << "Hello, the world of multiplications!\n\n";
+
+
+    // we need to wrap the meaningful part of the code in the do .. while loop statement block
+    // indentation matters!
+
+    // here we will track the user's answer answer about repeating a program
+    char userAnswer;
+
+    do
+    {
+        //char userAnswer;        // this object being defined here is not visiable (accessible) outside of the scope { }
+
+        // the main part of the multiplication algorithm
+        uint16_t m, n;           // using a byte is enough
+        cout << "Input 1 <= (m, n) <= 8: ";
+        cin >> m >> n;          // cin >> (m, n)
+
+        // the outter loop
+        for (uint16_t i = 0; i < m; ++i)
+        {
+            // the inner loop
+            for (uint16_t j = 0; j < n; ++j)
+            {
+                cout << i * j; // << '\t';
+                if (j < n - 1)
+                    cout << '\t';
+            }
+            cout << '\n';
+        }
+
+        cout << "Press Y to repeat the program: ";
+        cin >> userAnswer;
+
+    } while (userAnswer == 'Y' || userAnswer == 'y');
 
     return 0;
 }
--- code/src/problem4_len_of_cstr/main.cpp
+++ code/src/problem4_len_of_cstr/main.cpp
@@ -8,15 +8,100 @@
 ///             to programming" provided by the Faculty of Computer Science
 ///             at the Higher School of Economics.
 ///
-/// Count the length of a C-style string variable initialized with a string
+/// Count the length of a C-style string initialized with a string
 /// literal (implement strlen() ).
 ///
 ////////////////////////////////////////////////////////////////////////////////
 
 
+#include <cstddef>      // size_t
+#include <iostream>
+
+
+// fun example only
+namespace MyOwnNamespace {
+
+    int ALMOST_GLOBAL_OBJ;
+    int min(int a, int b)
+    {
+        if (a < b)
+            return a;
+        return b;
+    }
+
+};
+
+
+//int cin;
+
+using namespace std;    // can be used in a CPP-module, however it's better to
+                        // consider only needed components with the using statement
+                        // **MUST NOT** be used in any header file (.h, .hpp)
+//using std::cout;
+
+//using namespace MyOwnNamespace;
+//using MyOwnNamespace::min;
+
 int main()
 {
-    // TODO: input your code here
+
+    //MyOwnNamespace::ALMOST_GLOBAL_OBJ;
+//      MyOwnNamespace::min(10, 20);      // the function defined above by the programmer is called
+//                      min(10, 20);      // the stadard version of the min function defined in some of the standard header is called
+
+    // create some STATIC arrays
+    char arr1[4] = { 'a', 'b', 'c', '1' };
+    char arr2[6] = { '2', '3', '4'};
+    char arr3[] = { 'x', 'y', 'z'};
+    // ^ char means the datatype for individual elements of the array
+
+    // the trick (the approach) to get the real size of a static array:
+    size_t arr2Size = sizeof(arr2) / sizeof(arr2[0]);   // can use datatype instead of 1st el
+    size_t arr3Size = sizeof(arr3) / sizeof(char);      // can use 1st el instead of datatype
+
+    int arr4[] = { 10, 20, 42 };
+    //^ int means the datatype for individual elements of the array
+    size_t arr4Size = sizeof(arr4) / sizeof(arr4[0]);
+
+
+    // c-style strings are the (actually) strings that are defined by using string literal constans
+    char str1[] = "Abc";    // we initialize a array of chars using a string literal, so we created a c-string
+                            // the number of elements in this array is 3 (chars 'A', 'b' and 'c') + 1 (null terminator)
+    size_t str1Size = sizeof(str1) / sizeof(str1[0]);
+
+
+    // let's calculate the length of a c-string taking into account that after the last
+    // real character there will be put the 0-terminator (null symbol)
+    // we have implemented a function called strlen()
+
+    size_t i = 0;           // we use it to count the number of characters in the given string
+    char ch = str1[0];
+    while (ch != '\0')
+    {
+//        ++i;
+//        ch = str1[i];
+
+        ch = str1[++i];     // we can't use i++ expression instead, because the postfix
+                            // version return the previous value of i, but need the newest value,
+                            // the previous one has already been mentioned above.
+    }
+
+    // TODO: HW: remaster the fragment above (calculating the length of a string)
+    // using the for-loop statement.
+
+    // char str2[80];
+    //std::cin >> str2;     // the size of str1 is fixed and the user's input
+    // **MUST NOT** exceed the allocated space
+
+    int x = 1;
+    int y = 2;
+    int z = 3;
+
+    z = y = x;
+    // foo (y = x)
+    // logic (y == x)   // accidentely it's often used improperly: logic (y = x)
+
+
 
     return 0;
 }
